8721569f5e02eecb57c74a0c299f0cf0a43ed801
Lourdes <lourdes.rm10@gmail.com>
2019-12-18 21:47:08 -0600

    flask home page showing available routes

--- app.py
+++ app.py
@@ -1,3 +1,27 @@
 # Import Flask
 from flask import Flask
 
+# Create an app
+app = Flask(__name__)
+
+
+# Define routes
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/station<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/startdate<br/>"
+        f"/api/v1.0/enddate<br/>"
+)
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
